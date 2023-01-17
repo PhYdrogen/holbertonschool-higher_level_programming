d17076c384e1fa4dce37b31adb0149ce1ca75958
Gabriel <fossecruor@gmail.com>
2023-01-17 02:05:26 -0800

    Adding a case if i have 1 arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,5 +6,9 @@ if __name__ == "__main__":
         print("{} arguments:".format((nb_elem)))
         for i in range(1, nb_elem + 1):
             print("{}: {}".format(i, sys.argv[i]))
+    if nb_elem == 1:
+        print("1 argument:")
+        for i in range(1, nb_elem + 1):
+            print("{}: {}".format(i, sys.argv[i]))
     else:
         print("0 arguments.")
