ec9fd15099020b96279cc3387d37dae16be58160
Gabriel <fossecruor@gmail.com>
2023-01-17 02:02:11 -0800

    adding content for file 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    nb_elem = len(sys.argv) - 1
+    if nb_elem > 0:
+        print("{} arguments:".format((nb_elem)))
+        for i in range(1, nb_elem + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("0 arguments.")
