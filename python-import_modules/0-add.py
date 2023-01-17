805243cd9a06d96e35c79706f61f4ebb6c2d3af1
Gabriel <fossecruor@gmail.com>
2023-01-17 01:15:02 -0800

    init repo

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a=1, b=2):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    print("{} + {} = {}".format(a, b, a + b))
+    return (a + b)
