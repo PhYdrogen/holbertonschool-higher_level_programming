e95d9404d32e5f3f5f4bd794d0d8908ea258a427
Gabriel <fossecruor@gmail.com>
2023-01-17 01:51:44 -0800

    Fix, typo

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import calculator_1 as calc
     a = 10
     b = 5
-    prinf("{} {} {}".format(a, b, add(a, b)))
-    prinf("{} {} {}".format(a, b, sub(a, b)))
-    prinf("{} {} {}".format(a, b, mul(a, b)))
-    prinf("{} {} {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
