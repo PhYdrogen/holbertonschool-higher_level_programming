8b7e97e40be83e2e3379ebf416372bc27ed3af64
Gabriel <fossecruor@gmail.com>
2023-01-17 01:25:41 -0800

    importing module directly into my file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,5 @@
-#!/usr/bin/python3
-def add(a=1, b=2):
-    """My addition function
+#!/bin/usr/python3
+import add
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    print("{} + {} = {}".format(a, b, a + b))
-    return (a + b)
+if __main__:
+    print("{} + {} = {}".format(1, 2, add.add(1, 2)))
