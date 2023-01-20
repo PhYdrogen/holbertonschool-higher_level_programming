bc19815968bd6a47a04f96c08851d393762cb8e6
Gabriel <fossecruor@gmail.com>
2023-01-20 01:15:15 -0800

    adding advanced content

---
+++ python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sys.stderr.write("and that piece of art is useful -")
+    sys.stderr.write(" Dora Korpar, 2015-10-19\n")
+    sys.exit(1)
