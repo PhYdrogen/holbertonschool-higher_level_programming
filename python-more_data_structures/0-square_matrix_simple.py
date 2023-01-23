d86b2768833a4526355c60324fceb9d64c40626d
Gabriel <fossecruor@gmail.com>
2023-01-23 06:59:16 -0800

    Dynamtic len

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in range(len(matrix)):
         new_matrix_row = []
-        for j in range(len(matrix)-1):
+        for j in range(len(matrix[0])):
             act = matrix[i][j]
             new_matrix_row.append(act * act)
         new_matrix.append(new_matrix_row)
