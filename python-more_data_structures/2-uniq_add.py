a2d61913d40ffaf2a1661f9ace595da2e7f82e67
Gabriel <fossecruor@gmail.com>
2023-01-23 08:07:37 -0800

    I could use set for dict for real

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    didi = {0: 0}
+    res = 0
+    for i in my_list:
+        didi[i] = i
+
+    for x in didi.keys():
+        res += x
+
+    return res
