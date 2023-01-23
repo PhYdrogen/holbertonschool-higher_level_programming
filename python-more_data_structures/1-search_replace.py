fec3a39e52ab1d7cf34ed87e4c3d5e7bc8bb466d
Gabriel <fossecruor@gmail.com>
2023-01-23 07:32:37 -0800

    No map = easy

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_array = []
+    for i in my_list:
+        if i == search:
+            new_array.append(replace)
+        else:
+            new_array.append(i)
+    return new_array
