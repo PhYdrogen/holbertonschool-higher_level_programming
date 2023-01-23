d2ff2a2946db869db8c3e9df0b46268463f05f5a
Gabriel <fossecruor@gmail.com>
2023-01-23 04:07:08 -0800

    Remove the print, adding a return

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
-        print()
+        return
     else:
         my_list.reverse()
         for i in my_list:
