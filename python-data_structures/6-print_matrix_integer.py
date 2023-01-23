aa5e56d130ce1f2db907cc3c5748f143fef7255f
Gabriel <fossecruor@gmail.com>
2023-01-23 03:54:40 -0800

    adding conditionnal for end print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            end = "" if matrix[i][-1] == matrix[i][j] else " "
+            print("{:d}".format(matrix[i][j]), end=end)
         print()
