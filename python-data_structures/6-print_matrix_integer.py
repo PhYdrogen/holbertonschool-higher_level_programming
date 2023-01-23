a83884411f29ac5fb32fe2a4ad466a3eeba7166f
Gabriel <fossecruor@gmail.com>
2023-01-23 03:41:11 -0800

    Adding :d for format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print("{}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
