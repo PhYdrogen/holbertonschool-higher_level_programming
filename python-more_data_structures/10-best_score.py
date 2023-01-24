515519f80dd2e8a6879823a77d4ab9bfeca65dec
Gabriel <fossecruor@gmail.com>
2023-01-24 00:54:41 -0800

    Adding content for file 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max_value = 0
+    max_key = ''
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > max_value:
+            max_key = i
+    return max_key
