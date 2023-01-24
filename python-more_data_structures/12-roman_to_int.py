eace115676065f97c94edfba6141cb6f96f00b57
Gabriel <fossecruor@gmail.com>
2023-01-24 02:22:43 -0800

    IV works but nor II

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict_rom = dict(I=1, IV=4, V=5, IX=9, X=10, L=50, XC=90,
+                    C=100, D=500, CM=900, M=1000)
+    resultat = 0
+    char = ''
+    spec = ''
+    j = 0
+    for i in range(len(roman_string)):
+        if j >= len(roman_string):
+            break
+        char = roman_string[j]
+        if roman_string[j] == 'I' and j + 1 < len(roman_string):
+            spec = '{}{}'.format(char, roman_string[j+1])
+            if spec in dict_rom:
+                resultat += dict_rom[spec]
+                j += 2
+                continue
+        else:
+            resultat += dict_rom[char]
+            j += 1
+    return resultat
