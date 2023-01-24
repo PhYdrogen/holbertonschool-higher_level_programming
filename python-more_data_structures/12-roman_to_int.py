bd244b46606304b50cf2bb6f81142db9f7b16378
Gabriel <fossecruor@gmail.com>
2023-01-24 02:39:55 -0800

    Reformated and cleaner

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,21 +2,14 @@
 def roman_to_int(roman_string):
     dict_rom = dict(I=1, IV=4, V=5, IX=9, X=10, L=50, XC=90,
                     C=100, D=500, CM=900, M=1000)
+    last_char = ''
     resultat = 0
-    char = ''
-    spec = ''
-    j = 0
-    for i in range(len(roman_string)):
-        if j >= len(roman_string):
-            break
-        char = roman_string[j]
-        if roman_string[j] == 'I' and j + 1 < len(roman_string):
-            spec = '{}{}'.format(char, roman_string[j+1])
-            if spec in dict_rom:
-                resultat += dict_rom[spec]
-                j += 2
-                continue
-        else:
-            resultat += dict_rom[char]
-            j += 1
+    for i in roman_string:
+        x = '{}{}'.format(last_char, i)
+        if x in dict_rom and last_char in dict_rom:
+            resultat += dict_rom[x]
+            resultat -= dict_rom[last_char]
+        elif i in dict_rom:
+            resultat += dict_rom[i]
+        last_char = i
     return resultat
