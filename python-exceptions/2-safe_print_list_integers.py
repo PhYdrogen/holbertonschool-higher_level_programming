7f4a3687045cb106edbb299f0b38ebdc83cad6ab
Gabriel <fossecruor@gmail.com>
2023-01-24 06:23:07 -0800

    Adding content for file 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        count += 1
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            count -= 1
+            pass
+    print()
+    return count
