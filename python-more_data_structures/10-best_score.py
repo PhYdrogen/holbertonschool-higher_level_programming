c1fdcede2f8e0d7e6b2448c40ad6f2da34d4a5c9
Gabriel <fossecruor@gmail.com>
2023-01-24 00:59:38 -0800

    Adding update on max_value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,8 @@ def best_score(a_dictionary):
         return None
     max_value = 0
     max_key = ''
-    for i in a_dictionary.keys():
-        if a_dictionary[i] > max_value:
-            max_key = i
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > max_value:
+            max_key = key
+            max_value = a_dictionary[key]
     return max_key
