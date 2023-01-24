335d74a89086a38d36995eec1c142cb931c0ccbd
Gabriel <fossecruor@gmail.com>
2023-01-24 05:49:17 -0800

    Adding content for file 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+
+        print()
+
+    except IndexError:
+        print()
+        i -= 1
+    finally:
+        return i + 1
