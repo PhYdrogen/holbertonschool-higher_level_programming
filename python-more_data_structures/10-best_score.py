e88ee7f0bc10f139a12c5e452a4a7bd25bd2ac88
Gabriel <fossecruor@gmail.com>
2023-01-24 01:02:04 -0800

    Adding case for empty dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     max_value = 0
     max_key = ''
