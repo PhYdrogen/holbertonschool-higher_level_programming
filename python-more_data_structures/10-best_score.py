728d8caaaec23813810ddc02771e1e4391f44b49
PhYdrogen <Fossecruor@gmail.com>
2023-01-26 15:57:12 +0100

    Update 10-best_score.py
    Stronger, smarter, better

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
-    max_value = 0
-    max_key = ''
-    for key in a_dictionary.keys():
-        if a_dictionary[key] > max_value:
-            max_key = key
-            max_value = a_dictionary[key]
-    return max_key
+    sort_dict = sorted(a_dictionary.items())
+    return (sort_dict[-1][0])
