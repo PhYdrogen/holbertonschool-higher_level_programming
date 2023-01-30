f9c9c39213b52f8b8c4fbe9c574dfce92f9f7f9e
Gabriel <fossecruor@gmail.com>
2023-01-30 06:55:20 -0800

    adding content for file 2é

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" This is my doc square file
+"""
+
+
+class Square:
+    """ This is my square class
+    """
+
+    def __init__(self, size=0):
+        """ this is my init
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
