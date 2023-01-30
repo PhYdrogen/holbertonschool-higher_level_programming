d35cc269890431cc697748100b4fc365a3119857
Gabriel <fossecruor@gmail.com>
2023-01-30 06:41:17 -0800

    adding content for file 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Square doc
+"""
+
+
+class Square:
+    """ The class for Square
+    """
+    __size = 0
+
+    def __init__(self, size=0):
+        """ The module for creating a square
+        """
+        self.size = size
