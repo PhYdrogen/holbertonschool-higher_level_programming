f2e7d1f10493bda02ee5c1ed8d3163ed947f17e0
Gabriel <fossecruor@gmail.com>
2023-01-31 02:33:21 -0800

    adding more condition for the tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,7 +45,7 @@ class Square:
     def position(self, value):
         """ Pos Setter
         """
-        if type(value) is not tuple or value[0] < 0 and value[1] < 0:
+        if type(value) is not tuple or len(value) < 2 or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
