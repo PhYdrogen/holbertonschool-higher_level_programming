1049d3dedfc82f7db32cbdebcb0e4685eb2eac2c
Gabriel <fossecruor@gmail.com>
2023-01-31 01:29:28 -0800

    huh, fixed the module

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,23 +10,20 @@ class Square:
     def __init__(self, size=0):
         """ this is my init
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         """ Return the current square of the area of the square
         """
         return self.__size * self.__size
 
+    @property
     def size(self):
         """ Getter
         """
         return self.__size
 
+    @size.setter
     def size(self, value):
         """ Setter
         """
