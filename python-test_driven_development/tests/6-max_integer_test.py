973f13a830f4a78ef66f0738f4452e370f7a87ca
Gabriel <fossecruor@gmail.com>
2023-02-01 08:53:33 -0800

    adding content for unitest file

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Doc for Unittest
+    """
+
+    def maxatend(self):
+        """Test if last is max
+        """
+        test = [1, 2, 3]
+        self.assertEqual(max_integer(test), 3)
