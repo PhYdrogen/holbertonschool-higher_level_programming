679160833948635cf07a88a3c60734b1dfff8424
Gabriel <fossecruor@gmail.com>
2023-02-01 07:43:52 -0800

    adding content

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Doc
+"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for ligne in range(size):
+        for dieze in range(size):
+            print('#', end="")
+        print()
