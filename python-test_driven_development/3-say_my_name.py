4c45587f618dd8f4faf3d5e0fedd1247ee9d685c
Gabriel <fossecruor@gmail.com>
2023-02-01 06:55:54 -0800

    adding test and doc comment

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" Doc
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ Doc
+    """
+
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
