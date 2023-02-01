9a88da7c0046ec3c114a9ceee422be49b4684d5c
Gabriel <fossecruor@gmail.com>
2023-02-01 05:17:09 -0800

    adding content

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" Doc
+"""
+
+
+def matrix_divided(matrix, div):
+    """ Doc
+    """
+    if (type(div) is not int) and (type(div) is not float):
+        raise TypeError('div must be a number')
+    if matrix is None or type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+
+    len_matrix = len(matrix)
+    new_matrix = []
+    for list_row in matrix:
+        new_row_matrix = []
+        for elem in list_row:
+            try:
+                rep = elem / div
+                new_row_matrix.append(round(rep, 2))
+            except ZeroDivisionError:
+                raise ZeroDivisionError('division by zero')
+        new_matrix.append(new_row_matrix)
+
+    return new_matrix
