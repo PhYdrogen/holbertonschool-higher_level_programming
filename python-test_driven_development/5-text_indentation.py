33dac7052424e054a79f87ab41923c71e97cc644
Gabriel <fossecruor@gmail.com>
2023-02-01 08:12:03 -0800

    adding test and update file

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,10 +9,15 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
-    these = ['.', '?', ';']
+    these = ['.', '?', ':']
+    last_char = ''
     for char in text:
         if char in these:
             print(char)
             print()
+            last_char = '\n'
         else:
+            if last_char == '\n' and char == ' ':
+                continue
             print(char, end="")
+            last_char = char
