5b8e5bfa2d823e1b8a913bb46c72d9b5ab2bb5c6
Gabriel <fossecruor@gmail.com>
2023-02-01 04:31:35 -0800

    no idea commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,10 +5,6 @@
 
 def add_integer(a, b=98):
     """ Doc """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
