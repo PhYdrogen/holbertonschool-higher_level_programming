ba16cd4676f0d6c87725f9e2db1a53848290903f
Gabriel <fossecruor@gmail.com>
2023-02-01 09:35:30 -0800

    adding

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -25,8 +25,44 @@ class TestMaxInteger(unittest.TestCase):
     def test_maxatend(self):
         """Test if last is max
         """
-        test = [1, 2, 3]
-        self.assertEqual(max_integer(test), 3)
+        test1 = [1, 2, 3]
+        self.assertEqual(max_integer(test1), 3)
+
+    def test_maxatfront(self):
+        """Test
+        """
+        test2 = [3, 2, 0]
+        self.assertEqual(max_integer(test2), 3)
+
+    def test_maxatmid(self):
+        """Test
+        """
+        test3 = [1, 3, 2]
+        self.assertEqual(max_integer(test3), 3)
+
+    def test_one_negative(self):
+        """Test
+        """
+        test4 = [-1, 3, 2]
+        self.assertEqual(max_integer(test4), 3)
+
+    def test_only_negative(self):
+        """Test
+        """
+        test5 = [-1, -3, -2]
+        self.assertEqual(max_integer(test5), -1)
+
+    def test_one_elem(self):
+        """Test
+        """
+        test6 = [3]
+        self.assertEqual(max_integer(test6), 3)
+
+    def test_list_empty(self):
+        """Test
+        """
+        test7 = []
+        self.assertEqual(max_integer(test7), None)
 
 
 if __name__ == '__main__':
