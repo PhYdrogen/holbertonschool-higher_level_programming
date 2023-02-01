ff1ff3b4e981be0fd8d300719761d7a4b1b47305
Gabriel <fossecruor@gmail.com>
2023-02-01 02:27:30 -0800

    update my file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,18 +4,16 @@
 
 
 def add_integer(a, b=98):
-    """ This function add two number
-    """
-
-    if type(a) is float or type(a) is int:
-        if type(b) is float or type(b) is int:
-            return int(a) + int(b)
-        else:
-            raise TypeError('b must be an integer')
-    else:
-        raise TypeError('a must be an integer')
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
+    """ Doc """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
