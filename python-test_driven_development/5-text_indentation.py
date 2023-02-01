977d10811cc8e85c7d4791ea8a6b19d167c6459a
Gabriel <fossecruor@gmail.com>
2023-02-01 08:01:48 -0800

    Louange à ma créatrice

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Doc
+"""
+
+
+def text_indentation(text):
+    """ Doc
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    these = ['.', '?', ';']
+    for char in text:
+        if char in these:
+            print(char)
+            print()
+        else:
+            print(char, end="")
