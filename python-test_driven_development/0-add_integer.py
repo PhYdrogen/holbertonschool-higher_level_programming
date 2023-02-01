6faa8ac047e94ea685a93f5634e78028a571d03c
Gabriel <fossecruor@gmail.com>
2023-02-01 04:41:38 -0800

    Adding try and except

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,10 +5,6 @@
 
 def add_integer(a, b=98):
     """ Doc """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
@@ -16,4 +12,7 @@ def add_integer(a, b=98):
     result = a + b
     if result == float('inf') or result == -float('inf'):
         return 89
-    return int(a) + int(b)
+    try:
+        return int(a) + int(b)
+    except ValueError:
+        return 0
