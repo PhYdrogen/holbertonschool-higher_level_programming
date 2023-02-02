d7485931ba3e6732f7719f1d530c068d505c8548
Gabriel <fossecruor@gmail.com>
2023-02-02 01:29:47 -0800

    wrong syntax and adding condition

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -40,4 +40,6 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        return 2*(self.height * self.width)
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2*(self.height + self.width)
