f22b96cf316edc465b30ce7e06654687cee77167
Gabriel <fossecruor@gmail.com>
2023-02-07 10:47:49 -0800

    removing BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,25 +3,6 @@
 """
 
 
-class BaseGeometry:
-    """ Doc
-    """
-
-    def __init__(self):
-        pass
-
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) is bool:
-            raise TypeError('{} must be an integer'.format(name))
-        if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
 class Rectangle(BaseGeometry):
     """ Rectangle class of base geometry
     """
