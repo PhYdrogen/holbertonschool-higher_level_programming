d6f0619c69a4d5f0a9d053d4709a14932123e8c6
Gabriel <fossecruor@gmail.com>
2023-02-07 10:35:01 -0800

    adding content for file 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" File for empty class
+"""
+
+
+class BaseGeometry:
+    """ Doc
+    """
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is bool:
+            raise TypeError('{} must be an integer'.format(name))
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+    class Rectangle(BaseGeometry):
+        def __init__(self, width, height):
+            self.width = super().intgeger_validator(width)
+            self.height = super().intgeger_validator(height)
