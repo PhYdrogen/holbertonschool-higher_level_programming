6d809c3fd727f3a45c0b3b1cc570456993c5d35f
Gabriel <fossecruor@gmail.com>
2023-02-07 10:31:22 -0800

    adding test for True

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,6 +14,8 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        if type(value) is bool:
+            raise TypeError('{} must be an integer'.format(name))
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
