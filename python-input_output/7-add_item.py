eedea13d25e8445fb02a5450a1086c8e1c005483
Gabriel <fossecruor@gmail.com>
2023-02-08 06:17:49 -0800

    adding content for file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" main
+"""
+
+if __name__ == '__main__':
+    import sys
+    from os import path
+
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+    file = "add_item.json"
+    exist_file = path.exists(file)
+    if exist_file:
+        pass
+    else:
+        save_to_json_file([], file)
+
+    if len(sys.argv) == 1 and not exist_file:
+        save_to_json_file([], file)
+    elif len(sys.argv) < 2:
+        pass
+    else:
+        data_as_obj = load_from_json_file(file)
+        data_as_obj.append(sys.argv[1])
+        save_to_json_file(data_as_obj, file)
