81884b1ef21ddeb7313c5784e40896911639243a
Gabriel <fossecruor@gmail.com>
2023-02-08 08:23:37 -0800

    put some case and loop it in else

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,14 +13,16 @@ def pascal_triangle(n):
 
     matrix = []
     for n in range(n):
-        for chffire in range(n):
-            matrix_row = []
-            if n == 0 or n == 1:
-                matrix_row.append(1)
-            for o in len(matrix[n-1] - 1):
-                res = matrix[n-1][o] + matrix[n-1][o+1]
-                matriw_row.append(res)
-            matrix_row.insert(0, 1)
+        matrix_row = []
+        if n == 0:
             matrix_row.append(1)
+        elif n == 1:
+            matrix_row.append(1)
+            matrix_row.append(1)
+        else:
+            for x in range(n-1):
+                matrix_row.append(matrix[n-1][x] + matrix[n-1][x+1])
+            matrix_row.append(1)
+            matrix_row.insert(0, 1)
         matrix.append(matrix_row)
     return matrix
