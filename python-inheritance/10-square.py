e1ff74386f162d2b4a7ce74078c9614cb6fee423
Gabriel <fossecruor@gmail.com>
2023-02-08 01:15:40 -0800

    adding content for file 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" File for rectangle class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class of base Rectangle
+    """
+    def __init__(self, size):
+        """ init square
+        """
+        try:
+            super().integer_validator("size", size)
+            self.__size = size
+
+        except Exception as e:
+            print(e)
+
+    def area(self):
+        return self.size ** 2
