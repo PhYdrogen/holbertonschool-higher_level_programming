48710a8f4e96d04f784c80e11c0185b088b117c3
Gabriel <fossecruor@gmail.com>
2023-02-08 02:20:23 -0800

    adding content for file 2é

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
+def append_write(filename="", text=""):
+    """ append to a file fn
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        char_written = f.write(text)
+    f.close()
+    return char_written
