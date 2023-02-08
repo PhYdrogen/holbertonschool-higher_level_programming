7f3d2f8875e866b81de5a918433a5cef5b316f80
Gabriel <fossecruor@gmail.com>
2023-02-08 05:22:12 -0800

    adding content for file 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
+def load_from_json_file(filename):
+    """ fn to load from file and translate to obj
+    """
+    with open(filename, 'r') as f:
+        read_data = f.read()
+    f.close()
+    return json.dumps(read_data)
