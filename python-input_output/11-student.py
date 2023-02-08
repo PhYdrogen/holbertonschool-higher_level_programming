360f167740b333cdf10753cfbce61ab181c7b8b9
Gabriel <fossecruor@gmail.com>
2023-02-08 07:16:55 -0800

    adding content for file 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
+class Student:
+    """ class for student
+    """
+    def __init__(self, fn, ln, age):
+        """ init method
+        """
+        self.first_name = fn
+        self.last_name = ln
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+        else:
+            dict_attrs = vars(self)
+            new_dict = dict()
+            for arg in attrs:
+                if arg in dict_attrs:
+                    new_dict[arg] = dict_attrs.get(arg)
+            return new_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if key == "first_name":
+                self.first_name = value
+            elif key == "last_name":
+                self.last_name = value
+            elif key == "age":
+                self.age = value
