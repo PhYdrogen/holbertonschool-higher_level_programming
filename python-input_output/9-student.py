9debacd6549351861ded81a5414f315eb59baca0
Gabriel <fossecruor@gmail.com>
2023-02-08 06:43:46 -0800

    adding content for file 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
+class Student:
+    """ class for student
+    """
+    def __init__(self, fn, ln, age):
+        """ init method
+        """
+        self.first_name = fn
+        self.last_name = fn
+        self.age = age
+
+    def to_json(self):
+        return vars(self)
