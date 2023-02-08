7f7a6e2175c9b4a7100a8d3ef988ca6bd1db44d5
Gabriel <fossecruor@gmail.com>
2023-02-08 02:08:19 -0800

    adding content for file 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
+def write_file(filename="", text=""):
+    """ write_file fn
+    """
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
+    f.close()
