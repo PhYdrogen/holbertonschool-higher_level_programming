72bf00b0bf23eda34b75e8af0340a4ddde7a865b
Gabriel <fossecruor@gmail.com>
2023-02-08 02:03:00 -0800

    doc is keykey

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" doc
+"""
+
+
 def read_file(filename=""):
+    """ read_file fn
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
