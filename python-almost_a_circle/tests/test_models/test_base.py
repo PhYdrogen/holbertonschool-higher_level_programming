8fa106e9135defca96e94b377f639fe93506934a
Gabriel <fossecruor@gmail.com>
2023-02-13 01:55:07 -0800

    adding tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_base_assign_id_auto_0(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_base_assign_id_auto_1(self):
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+    def test_base_assign_id_auto_2(self):
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
