8b5dd1b80d22f499a6c0b2724b3e9cff8b4a98aa
Gabriel <fossecruor@gmail.com>
2023-02-13 07:42:08 -0800

    adding doc for base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ use the json lib to convert python obj to json string """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
