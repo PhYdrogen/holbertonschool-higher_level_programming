a122932f867398ede9fde884fe82068977357245
Gabriel <fossecruor@gmail.com>
2023-02-13 01:38:57 -0800

    adding the base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" doc file base class
+"""
+
+
+class Base:
+    """ class base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
