6d6fcd75f6b2a6e25a2024c2000f07430a0b3076
Gabriel <fossecruor@gmail.com>
2023-02-13 07:45:08 -0800

    forget the @staticmethod in fn

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ use the json lib to convert python obj to json string """
         if list_dictionaries is None or len(list_dictionaries) == 0:
