7e2ef883da0222fbd83c22657cbeaf8e7ffb100a
Gabriel <fossecruor@gmail.com>
2023-02-13 06:40:28 -0800

    adding update fn to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,3 +25,30 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+        else:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                if count == 1:
+                    self.width = arg
+                if count == 2:
+                    self.height = arg
+                if count == 3:
+                    self.x = arg
+                if count == 4:
+                    self.y = arg
+                count += 1
+
