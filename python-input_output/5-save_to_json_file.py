078109c9782aa41e6eb0b8637dd71abdf269a948
Gabriel <fossecruor@gmail.com>
2023-02-13 03:27:04 -0800

    adding rectangle file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ create a new file
+    """ fn to write from python to a file
     """
     with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
