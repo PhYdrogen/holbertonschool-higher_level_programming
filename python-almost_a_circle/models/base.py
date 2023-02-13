ddb92721ab8e60d0a81f581a7980f82e250c70e9
Gabriel <fossecruor@gmail.com>
2023-02-13 07:41:01 -0800

    modify base.py for py > string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ doc file base class
 """
+import json
 
 
 class Base:
@@ -14,3 +15,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
