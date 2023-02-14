03e87b64203c9e24b892bd64a28173b353e1007a
Gabriel <fossecruor@gmail.com>
2023-02-14 05:44:00 -0800

    adding more test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -16,6 +16,30 @@ class TestBase(unittest.TestCase):
         b3 = Base(89)
         self.assertEqual(b3.id, 89)
 
+    def test_base_to_json_none(self):
+        b4 = Base.to_json_string(None)
+        self.assertEqual(b4, '[]')
+
+    def test_base_to_json_empty(self):
+        b5 = Base.to_json_string([])
+        self.assertEqual(b5, "[]")
+
+    def test_base_to_json_id_12(self):
+        b6 = Base.to_json_string([{'id': 12}])
+        self.assertEqual(b6, '[{"id": 12}]')
+
+    def test_base_from_json_none(self):
+        b7 = Base.from_json_string(None)
+        self.assertEqual(b7, [])
+
+    def test_base_from_json_empty(self):
+        b8 = Base.from_json_string("[]")
+        self.assertEqual(b8, [])
+
+    def test_base_from_json_id_89(self):
+        b9 = Base.from_json_string('[{ "id":89 }]')
+        self.assertEqual(b9, [{'id': 89}])
+
 
 if __name__ == '__main__':
     unittest.main()
