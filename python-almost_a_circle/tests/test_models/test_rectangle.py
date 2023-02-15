83c50ea97e79280cbb82414b661af18b4859368b
Gabriel <fossecruor@gmail.com>
2023-02-15 07:52:47 -0800

    Oui, je ne sais pas

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,13 +1,1 @@
 #!/usr/bin/python3
-import unittest
-from models.rectangle import Rectangle
-
-
-class TestRectangle(unittest.TestCase):
-    def test_basic(self):
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.id, 3)
-
-
-if __name__ == '__main__':
-    unittest.main()
