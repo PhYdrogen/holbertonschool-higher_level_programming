f5dc3a7fb5c67cf74bd9232c70cc01bd02c6ca85
Gabriel <fossecruor@gmail.com>
2023-02-16 01:03:26 -0800

    adding test rectangle

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestSquare(unittest.TestCase):
+    def test_rectangle_1(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(1)
+        self.assertTrue("Rectangle.__init__() missing 1 required positional argument: 'height'" in str(context.exception))
+
+    def test_rectangle_1_2(self):
+        r2 = Rectangle(1, 2)
+        self.assertEqual(r2.height, 2)
+
+    def test_rectangle_1_2_3(self):
+        r3 = Rectangle(1, 2, 3)
+        self.assertEqual(r3.x, 3)
+
+    def test_rectangle_1_2_3_4(self):
+        r3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r3.y, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
