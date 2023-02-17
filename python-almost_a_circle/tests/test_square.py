61db34e2f8cb016070e00b598ed7edca3a3c389e
PhYdrogen <Fossecruor@gmail.com>
2023-02-17 10:55:10 +0100

    import os, added teardown, new test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import unittest
+import os
 from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
+    @classmethod
+    def tearDown(self):
+        if os.path.exists("./Rectangle.json"):
+            os.remove("Rectangle.json")
+          
     def test_square_1(self):
         s1 = Square(1)
         self.assertEqual(s1.size, 1)
@@ -55,6 +61,10 @@ class TestSquare(unittest.TestCase):
             Square(0)
         self.assertTrue('width must be > 0' in str(context.exception))
 
+    def test_square_tp_dict(self):
+        s6 = Square(1, 2, 3)
+        self.assertEqual(type(s6.to_dictionary()), dict)
+      
     def test_square_update(self):
         s7 = Square(1, 2, 3)
         s7.update(89)
@@ -65,7 +75,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s7.x, 2)
         s7.update(89, 1, 2, 3)
         self.assertEqual(s7.y, 3)
-
+      
     def test_square_create(self):
         s8 = Square.create(**{'id': 89})
         self.assertEqual(s8.id, 89)
@@ -77,9 +87,30 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s8.y, 4)
 
     def test_str(self):
-        r9 = Square(1)
-        self.assertEqual("{}".format(r9), "[Square] ({}) 0/0 - 1".format(r9.id)) 
+        s9 = Square(1)
+        self.assertEqual("{}".format(s9), "[Square] ({}) 0/0 - 1".format(s9.id))
 
+    def test_rectangle_save(self):
+        Square.save_to_file(None)
+        with open("Square.json", "r") as file:
+          self.assertEqual(file.read(), '[]')
+        file.close()
+    
+    def test_rectangle_save_empty(self):
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+          self.assertEqual(file.read(), "[]")
+        file.close()
+      
+    def test_square_save_square(self):
+        s10 = Square(1, 0, 0, 13)
+        Square.save_to_file([s10])
+        with open("Square.json", "r") as file:
+          self.assertEqual(file.read(), '[{"id": 13, "size": 1, "x": 0, "y": 0}]')
+        file.close()
+
+    def test_load_square(self):
+        Square.load_from_file()
 
 if __name__ == '__main__':
     unittest.main()
