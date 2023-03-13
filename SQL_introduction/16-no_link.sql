0d860c9a8a68d70baf0e5fe40569a33cd53e034a
Gabriel <fossecruor@gmail.com>
2023-03-13 09:44:05 -0700

    ALL UPERCASE

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table of the database
-SELECT score, name from second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
