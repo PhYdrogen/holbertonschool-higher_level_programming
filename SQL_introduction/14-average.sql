a0ef68b13a67b7f5b5be1ee99b8501ac6ae68e7d
Gabriel <fossecruor@gmail.com>
2023-03-13 09:09:16 -0700

    change the result column

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- A script that computes the score average of all records in the table second_table
-INSERT INTO second_table SELECT AVG(score) AS score FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
