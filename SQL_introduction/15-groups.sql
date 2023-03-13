a2eb245e9a58b4883f504478910aad0b955ffa91
Gabriel <fossecruor@gmail.com>
2023-03-13 09:33:09 -0700

    I will continue uppercase

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- A script that lists the number of records with the same score in the table second_table
-select score, count(score) as number from second_table group by score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
