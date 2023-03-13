b60be640a5c9051724599d019aa4bd0fe689b4ab
Gabriel <fossecruor@gmail.com>
2023-03-13 10:18:18 -0700

    I was changing too much

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,7 @@
 -- Go to UTF8
-ALTER DATABASE
-      `hbtn_0c_0`
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-USE `hbtn_0c_0`;
-ALTER TABLE
-      `first_table`
-      CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-ALTER TABLE
-      `first_table`
-      CHANGE `name` `name`
-      VARCHAR(256)
-      COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0`
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+	CONVERT TO CHARACTER SET utf8mb4 
+	COLLATE utf8mb4_unicode_ci;
