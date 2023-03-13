1f8bac0bf910d11ebdeb5d41e550ba36877e869c
Gabriel <fossecruor@gmail.com>
2023-03-13 09:49:16 -0700

    no null name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- a script that lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name 
+	FROM second_table 
+	WHERE name IS NOT NULL
+	ORDER BY score DESC;
