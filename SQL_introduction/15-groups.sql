84b8a25b87cef5663e4f18e2aef75c6444ecd820
Gabriel <fossecruor@gmail.com>
2023-03-13 09:34:30 -0700

    group by desc

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- A script that lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
