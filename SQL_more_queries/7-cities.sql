7f3f474f3334aef346e8db77eb72f3a235fe8ac5
Gabriel <fossecruor@gmail.com>
2023-03-14 07:27:39 -0700

    adding content

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- A script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT FOREIGN KEY REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+	);
