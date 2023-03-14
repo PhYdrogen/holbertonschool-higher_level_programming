86238f5b59c9a47170dee79ad5248eafea779aaf
Gabriel <fossecruor@gmail.com>
2023-03-14 05:45:41 -0700

    pushing with quote

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON TABLE *.hbtn_0d_2 TO user_0d_2;
