14f861da7020e9992f75a994c4c177f19313f1ae
Gabriel <fossecruor@gmail.com>
2023-03-14 07:20:02 -0700

    not default omg

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- A script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT DEFAULT 1 UNIQUE NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id)
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
 	);
