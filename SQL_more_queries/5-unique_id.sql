57e53efec27193932d9d478ac0cfe14bf61b0138
Gabriel <fossecruor@gmail.com>
2023-03-14 06:56:18 -0700

    HPE

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- A script that creates the table unique_id
-CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
