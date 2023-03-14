ea727d94d59ac5842214afd9e9f6c3b0f2451146
Gabriel <Fossecruor@gmail.com>
2023-03-14 21:33:16 +0100

    ORDER BY CORRECT

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
