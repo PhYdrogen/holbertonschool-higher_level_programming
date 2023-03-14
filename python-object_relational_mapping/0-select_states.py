c1e54da77bda0eb648e8d2f850a3b450c72ec0b5
Gabriel <Fossecruor@gmail.com>
2023-03-14 22:42:06 +0100

    i have to push to test it

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,13 @@
 if __name__ == "__main__":
     import MySQLdb as DB
     import sys
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
-        db_connect = DB.connect(host="localhost", port=3306, 
-                                user="mysql_username", passwd="mysql_password", db="database_name")
+        db_connect = DB.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
         db_cursor = db_connect.cursor()
-        db_cursor.execute("SELECT * FROM states")
+        db_cursor.execute("SELECT * FROM states SORTED BY states.id")
         rows_selected = db_cursor.fetchall()
         for row in rows_selected:
             print(row)
