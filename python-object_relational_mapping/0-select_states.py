90e95f6258350aba6186d5de40eac2130ba8a031
Gabriel <Fossecruor@gmail.com>
2023-03-14 22:12:29 +0100

    adding content

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+import MySQLdb as DB
+# establish a database connection
+db_connect = DB.connect(host="localhost", port=3306, user="root", passwd="data", db="hbtn_0e_0_usa")
+# create a cursor to enable SQL queries
+db_cursor = db_connect.cursor()
+# execute SQL code
+db_cursor.execute("SELECT * FROM states")
+# accessing the data retrieved
+rows_selected = db_cursor.fetchall()
+for row in rows_selected:
+    print(row)
