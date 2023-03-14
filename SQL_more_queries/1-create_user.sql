f274e79b2d4a3206f1bd2af3e9d2acf9205a4876
Gabriel <fossecruor@gmail.com>
2023-03-14 03:16:58 -0700

    adding grant option

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `password`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
