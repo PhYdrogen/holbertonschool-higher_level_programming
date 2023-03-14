68310c62c9db324affd93abed20819896d2a7965
Gabriel <Fossecruor@gmail.com>
2023-03-14 21:15:29 +0100

    UPPERCASE

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@
 -- FROM tv_shows
 -- WHERE tv_shows.title = "Dexter"
 
-SELECT tv_genres.name as name
+SELECT tv_genres.name AS name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show_genres.show_id = 10
