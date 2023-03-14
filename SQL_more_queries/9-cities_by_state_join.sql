6752ea9e5bb9cf2774f027dda3df1be0284703a8
Gabriel <fossecruor@gmail.com>
2023-03-14 09:29:26 -0700

    adding content

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- A script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name
-FROM cities, states
+SELECT cities.id, cities.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
