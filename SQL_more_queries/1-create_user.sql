90f7bb401f7228ed53532321c443f8e85b346f97
Gabriel <fossecruor@gmail.com>
2023-03-14 03:09:28 -0700

    adding password and perms

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- create user
-CREATE USER `user_0d_1`@`localhost`;
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `password`;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
