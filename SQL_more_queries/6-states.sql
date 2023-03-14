656e52bb52d4edff95bba216aa5b6e1136c89f0f
Gabriel <fossecruor@gmail.com>
2023-03-14 07:03:45 -0700

    adding primary key as mysql type

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,8 @@
 -- A script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT DEFAULT 1 NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states 
+	id INT DEFAULT 1 NOT NULL UNIQUE,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
+	);
