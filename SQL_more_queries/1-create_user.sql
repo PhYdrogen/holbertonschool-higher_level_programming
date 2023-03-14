7bc71ffa40a5afc06ce01a255613ba3943a2f423
Gabriel <fossecruor@gmail.com>
2023-03-14 04:04:17 -0700

    adding good password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
