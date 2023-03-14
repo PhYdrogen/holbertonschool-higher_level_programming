cf4e3e2c1f8969621f4027cd21ad12c14471f34a
Gabriel <Fossecruor@gmail.com>
2023-03-14 18:34:07 +0100

    Adding comment

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-use hbtn_0d_tvshows;
+-- Write a script that lists all shows contained in
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
