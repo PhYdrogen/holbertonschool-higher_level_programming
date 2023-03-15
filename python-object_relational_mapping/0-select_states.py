4b19fb526c016136afaa732f791c2942c577d350
Gabriel <Fossecruor@gmail.com>
2023-03-15 01:37:46 +0100

    pycodestyle and more

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
-        db_connect = DB.connect(host="localhost", port=3306, user=mysql_username,
-                                passwd=mysql_password, db=database_name)
+        db_connect = DB.connect(host="localhost", port=3306,
+                                user=mysql_username,
+                                passwd=mysql_password,
+                                db=database_name)
         db_cursor = db_connect.cursor()
         db_cursor.execute("SELECT * FROM states ORDER BY states.id")
         rows_selected = db_cursor.fetchall()
---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" DOCUMENTATIONS """
+if __name__ == "__main__":
+    import MySQLdb as DB
+    import sys
+    if len(sys.argv) == 4:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+        db_connect = DB.connect(host="localhost",
+                                port=3306,
+                                user=mysql_username,
+                                passwd=mysql_password,
+                                db=database_name)
+        db_cursor = db_connect.cursor()
+        db_cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+        rows_selected = db_cursor.fetchall()
+        for row in rows_selected:
+            print(row)
