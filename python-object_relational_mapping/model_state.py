ca10b0e74de857d2e529c03555546ee4b0203f7b
Gabriel <Fossecruor@gmail.com>
2023-03-15 19:09:21 +0100

    remove the if

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 """ DOCUMENTATION """
 
-if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    Base = declarative_base()
-    class State(Base):
-        """ Class state that inherit of base 
-            from the task 6,
-            tablename is the table of the bd
-            id is id
-            and
-            name is name
-            :)
-        """
+Base = declarative_base()
+class State(Base):
+    """ Class state that inherit of base """
 
-        __tablename__ = 'states'
-        id = Column("id", Integer, nullable=False,unique=True,
-                    autoincrement="auto", primary_key=True)
-        name = Column("name", String(128), nullable=False)
+    __tablename__ = 'states'
+    id = Column("id", Integer, nullable=False,unique=True, autoincrement="auto", primary_key=True)
+    name = Column("name", String(128), nullable=False)
