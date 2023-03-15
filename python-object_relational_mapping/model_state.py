902367968e4c30191e3e521c4fbefe243e8d09ea
Gabriel <Fossecruor@gmail.com>
2023-03-15 19:04:03 +0100

    remove code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,21 +2,14 @@
 """ DOCUMENTATION """
 
 if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String, create_engine
+    from sqlalchemy import Column, Integer, String
     from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy.orm import sessionmaker
-    import sys
 
     Base = declarative_base()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     class State(Base):
-        """ Class state that inherit of base
-        """
+        """ Class state that inherit of base """
 
         __tablename__ = 'states'
         id = Column("id", Integer, nullable=False,unique=True,
                     autoincrement="auto", primary_key=True)
         name = Column("name", String(128), nullable=False)
-        def __init__(self):
-            """ THIS IS DOC """
-            pass
