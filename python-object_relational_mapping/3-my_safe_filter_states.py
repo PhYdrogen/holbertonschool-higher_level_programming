3273330920e74bb3c7f3aea049124e7101c574db
Gabriel <Fossecruor@gmail.com>
2023-03-15 02:30:08 +0100

    safe now

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,12 +3,16 @@
 if __name__ == "__main__":
     import MySQLdb as DB
     import sys
+
     if len(sys.argv) == 5:
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
         state_name_searched = sys.argv[4]
-        safe = state_name_searched.split(";")[0]
+        if state_name_searched.find(";") != -1:
+            safe = state_name_searched.split(";")[0][:-1]
+        else:
+            safe = state_name_searched
         db_connect = DB.connect(host="localhost",
                                 port=3306,
                                 user=mysql_username,
@@ -18,7 +22,7 @@ if __name__ == "__main__":
         db_cursor.execute(
             "SELECT * FROM states WHERE \
                 states.name LIKE BINARY '{}';"
-            .format(safe[1:-1])
+            .format(safe)
             )
         rows_selected = db_cursor.fetchall()
         for row in rows_selected:
