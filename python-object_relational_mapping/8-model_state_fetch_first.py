9fd270b55b0fa0f3bdf0640be568ae2acb0d209f
Gabriel <Fossecruor@gmail.com>
2023-03-15 20:59:53 +0100

    try to not fetch all the table

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     cursor = Session()
 
-    for col in cursor.query(State).filter(State.id == 1):
-        print(f'{col.id}: {col.name}')
+    for num, ville in cursor.query(State.id, State.name).filter(State.id == 1):
+        print(f'{num}: {ville}')
