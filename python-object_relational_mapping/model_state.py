f3de880c322522fd8a6e9c7a6e17072f75d3f7a3
Gabriel <Fossecruor@gmail.com>
2023-03-15 19:11:01 +0100

    pycode

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,9 +5,14 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     """ Class state that inherit of base """
 
     __tablename__ = 'states'
-    id = Column("id", Integer, nullable=False,unique=True, autoincrement="auto", primary_key=True)
+    id = Column("id", Integer,
+                 nullable=False,
+                 unique=True, autoincrement="auto",
+                 primary_key=True)
     name = Column("name", String(128), nullable=False)
