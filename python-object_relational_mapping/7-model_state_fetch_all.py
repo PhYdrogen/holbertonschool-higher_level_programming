9c8ae6ed4f4c7e48d7d6d48ed07526c8ebec1e53
Gabriel <Fossecruor@gmail.com>
2023-03-15 19:45:46 +0100

    Adding content

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" FIRST FETCH WITH PYTHON """
+
+if __name__ == "__main__":
+    
+    import sys
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    cursor = Session()
+    
+    for col in cursor.query(State).order_by(State.id):
+        print(f'{col.id}: {col.name}')
