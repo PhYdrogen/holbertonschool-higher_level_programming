fae9197ca68c3be45f1cc99741a11b279ef1d163
Gabriel <Fossecruor@gmail.com>
2023-03-15 19:12:14 +0100

    indent pycode

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,8 +11,6 @@ class State(Base):
     """ Class state that inherit of base """
 
     __tablename__ = 'states'
-    id = Column("id", Integer,
-                 nullable=False,
-                 unique=True, autoincrement="auto",
-                 primary_key=True)
+    id = Column("id", Integer, nullable=False, unique=True,
+autoincrement="auto", primary_key=True)
     name = Column("name", String(128), nullable=False)
