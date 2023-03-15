fdc0228a48c7f2a57a8ecc7a60dde1dda66b142c
Gabriel <Fossecruor@gmail.com>
2023-03-15 18:52:02 +0100

    added engine and if

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """ DOCUMENTATION """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-class State(Base):
-    """ Class state that inherit of base """
-    __tablename__ = 'states'
-    id = Column(Integer, nullable=False,unique=True,
-                autoincrement="auto", primary_key=True)
-    name = Column(String, nullable=False)
+if __name__ == "__main__":
+    from sqlalchemy import Column, Integer, String, create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    Base = declarative_base()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    class State(Base):
+        """ Class state that inherit of base """
+        __tablename__ = 'states'
+        id = Column("id", Integer, nullable=False,unique=True,
+                    autoincrement="auto", primary_key=True)
+        name = Column("name", String(128), nullable=False)
