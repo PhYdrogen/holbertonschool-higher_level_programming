c531c59cdfdaade07f5af5ddd1ba9e83aa5df3f7
Gabriel <fossecruor@gmail.com>
2023-04-09 08:18:03 -0700

    adding content

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+let userNum = process.argv[2];
+userNum = Number(userNum);
+if (isNaN(userNum)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + userNum);
+}
