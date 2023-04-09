51ef371557bbb42e69c69fdf28ebab90b3ad7791
Gabriel <fossecruor@gmail.com>
2023-04-09 08:07:17 -0700

    added variable instead of exit

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-import { exit } from 'node:process';
 
 let count = 0;
+let isFound = false;
 process.argv.forEach(element => {
   if (count === 2) {
     console.log(element);
-    exit(0);
+    isFound = true;
   }
   count += 1;
 });
-console.log('No argument');
+if (!isFound) {
+  console.log('No argument');
+}
