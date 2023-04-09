3d4f3b1d8b80198ecf0ac7a81104ee2997763ca9
Gabriel <fossecruor@gmail.com>
2023-04-09 08:03:04 -0700

    JEJ8

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const nbArg = process.argv.length;
 if (nbArg === 2) {
-    console.log('No argument');
-} else if (nbArg === 3){
-    console.log('Argument found');
+  console.log('No argument');
+} else if (nbArg === 3) {
+  console.log('Argument found');
 } else if (nbArg > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+import { exit } from 'node:process';
 
 let count = 0;
 process.argv.forEach(element => {
-    if (count === 2) {
-        console.log(element);
-        exit(0);
-    }
-    count += 1;
+  if (count === 2) {
+    console.log(element);
+    exit(0);
+  }
+  count += 1;
 });
 console.log('No argument');
