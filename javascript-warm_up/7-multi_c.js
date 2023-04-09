5e2d22f5c0241c8415da01ad7f1e27202af90b8b
Gabriel <fossecruor@gmail.com>
2023-04-09 08:29:41 -0700

    LGTM

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const userNb = Number(process.argv[2]);
+if (isNaN(userNb)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < userNb; i++) {
+    console.log('C is fun');
+  }   
+}
