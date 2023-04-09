aeb1c85f46f0c6c64ac625c19e34aa5347ae7231
Gabriel <fossecruor@gmail.com>
2023-04-09 09:04:47 -0700

    Adding content, not really sure

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const argv = process.argv;
+let numbers_list = argv.slice(2);
+numbers_list = numbers_list.sort();
+console.log(numbers_list[numbers_list.length - 2]);
+//console.log(numbers_list);
