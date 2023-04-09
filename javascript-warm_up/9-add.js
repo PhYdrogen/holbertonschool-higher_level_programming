63218b7a5dd49aef557256246b58df43bd303216
Gabriel <fossecruor@gmail.com>
2023-04-09 08:43:18 -0700

    adding content for file 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+function add (a, b) {
+  a = Number(a);
+  b = Number(b);
+  return (a + b);
+}
+
+const first = process.argv[2];
+const second = process.argv[3];
+console.log(add(first, second));
