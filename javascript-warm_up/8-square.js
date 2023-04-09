9ccc1c33209663ccf8d74c04130c676b99256877
Gabriel <fossecruor@gmail.com>
2023-04-09 08:36:10 -0700

    LGTM

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const userNb = Number(process.argv[2]);
+if (isNaN(userNb)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < userNb; i++) {
+    for (let j = 0; j < userNb; j++) {     
+      process.stdout.write('X');
+    }
+  console.log();
+  }
+}
