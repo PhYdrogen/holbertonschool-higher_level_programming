61962137a1ef41acff1cf74cfef2bba0b912cbe6
Gabriel <fossecruor@gmail.com>
2023-04-09 08:55:37 -0700

    semistandard js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (n === 1) {
-        return 1;
-    }
-    return (n * factorial(n-1));
+  if (n === 1) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
 }
 
 const argv = process.argv;
 let userNum = Number(argv[2]);
 if (isNaN(userNum)) {
-    userNum = 1;
+  userNum = 1;
 }
 console.log(factorial(userNum));
