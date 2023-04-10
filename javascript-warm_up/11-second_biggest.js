a92456a191bfb08850fa5bb8dbe0095ec9cd4e45
Gabriel <fossecruor@gmail.com>
2023-04-10 00:14:07 -0700

    jej8

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const argv = process.argv;
-const numbers_list = [1, 10, 11, 3, 4, 5];
+const numbersList = argv.slice(2);
 let big = 0;
 let second = 0;
-for (let i = 0; i < numbers_list.length; i++) {
-    if (i == 0) {
-        big = numbers_list[i];        
-    } else if (numbers_list[i] > big) {
-        second = big;
-        big = numbers_list[i];
-    }
+for (let i = 0; i < numbersList.length; i++) {
+  if (i === 0) {
+    big = numbersList[i];
+  } else if (numbersList[i] > big) {
+    second = big;
+    big = numbersList[i];
+  }
 }
 console.log(second);
