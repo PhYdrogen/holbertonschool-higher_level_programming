cb3db7a610c42fb1a260b259c53288e489c4cd31
Gabriel <fossecruor@gmail.com>
2023-04-10 00:25:21 -0700

    remove loop added sort with fn

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 const argv = process.argv;
 const numbersList = argv.slice(2);
-let big = 0;
-let second = 0;
-for (let i = 0; i < numbersList.length; i++) {
-  if (i === 0) {
-    big = numbersList[i];
-  } else if (numbersList[i] > big) {
-    second = big;
-    big = numbersList[i];
-  }
-}
-console.log(second);
+numbersList.sort(function(a, b){return a - b})
+console.log(numbersList[numbersList.length - 2]);
