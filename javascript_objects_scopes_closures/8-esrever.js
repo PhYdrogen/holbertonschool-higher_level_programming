c8acfec3b275c760cb6acc9991b9749dbb6417e2
Gabriel <fossecruor@gmail.com>
2023-04-10 05:43:32 -0700

    add contentn

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+function esrever (list) {
+  const newList = new Array();
+  for (let i = list.length - 1; i >= 0 ; i--){
+    newList.push(list[i]);
+  }
+  return newList;
+}
+module.exports = { esrever };
