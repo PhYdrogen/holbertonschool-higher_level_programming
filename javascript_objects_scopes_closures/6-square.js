e4846b8f6aba680cb910027cfbb7cfc5ad77c813
Gabriel <fossecruor@gmail.com>
2023-04-10 05:22:50 -0700

    add content

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let char = ''
+    if (c === undefined){
+      char = 'X';
+    } else {
+      char = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(char);
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Square;
