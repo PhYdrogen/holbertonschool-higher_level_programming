34ac2d71b8b29620cc9f3b71b00af3d420cc4ddb
Gabriel <fossecruor@gmail.com>
2023-04-10 04:55:41 -0700

    add content for file 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
+      //
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Rectangle;
