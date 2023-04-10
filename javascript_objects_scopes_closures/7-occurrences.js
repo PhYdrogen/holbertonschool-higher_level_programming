4493c7090de70c1f3f436a00f05019b13708d17a
Gabriel <fossecruor@gmail.com>
2023-04-10 05:32:20 -0700

    add content

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+function nbOccurences (list, searchElement) {
+  let nb = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      nb += 1;
+    }
+  }
+  return nb;
+}
+module.exports = { nbOccurences };
