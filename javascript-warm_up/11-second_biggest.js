a6825249f55a53f508a5c0ad21f529132028085b
Gabriel <fossecruor@gmail.com>
2023-04-10 00:39:19 -0700

    Added if else

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
-const numbersList = argv.slice(2);
-numbersList.sort(function(a, b){return a - b})
-console.log(numbersList[numbersList.length - 2]);
+if (argv.length === 2 || argv[2] === '1') {
+  console.log('0');
+} else {
+  const numbersList = argv.slice(3);
+  numbersList.sort(function (a, b) { return a - b; });
+  console.log(numbersList[numbersList.length - 2]);
+}
