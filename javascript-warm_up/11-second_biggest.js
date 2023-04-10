03baa0243f08204de6a813591d27b7551c0d81c0
Gabriel <fossecruor@gmail.com>
2023-04-10 00:45:29 -0700

    fix ?

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv.length === 2 || argv[2] === '1') {
+if (argv.length === 2 || argv[2] === '1' && argv.length < 3) {
   console.log('0');
 } else {
-  const numbersList = argv.slice(3);
+  const numbersList = argv.slice(2);
   numbersList.sort(function (a, b) { return a - b; });
   console.log(numbersList[numbersList.length - 2]);
 }
