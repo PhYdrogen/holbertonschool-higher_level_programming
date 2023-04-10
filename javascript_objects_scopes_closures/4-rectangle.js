6ea8d5633d4fa55711369c56cb88d8e693cdddd7
Gabriel <fossecruor@gmail.com>
2023-04-10 04:59:18 -0700

    added content for file 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
+      //
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
