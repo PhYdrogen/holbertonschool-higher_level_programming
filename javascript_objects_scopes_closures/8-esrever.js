4b06bbe9e369943f52a38edb10a05c3d0ea883d6
Gabriel <fossecruor@gmail.com>
2023-04-10 05:43:47 -0700

    fixed

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function esrever (list) {
   const newList = new Array();
-  for (let i = list.length - 1; i >= 0 ; i--){
+  for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
   return newList;
