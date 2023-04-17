22c65c301467d18a866f8486a3817b330960d395
Gabriel <fossecruor@gmail.com>
2023-04-17 04:06:23 -0700

    Added the core logic of the script

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const req = require('request');
+
+req(process.argv[2], (err, result, body) => {
+  let stats = {}; 
+  const data = JSON.parse(body);
+  data.forEach(obj => {
+    if (obj.completed === true) {
+      if (isNaN(stats[obj.userId])){
+        stats[obj.userId] = 1;
+      }
+      else {
+        stats[obj.userId] = stats[obj.userId] + 1;
+      }
+    }
+    
+  });
+  console.log(stats);
+});
