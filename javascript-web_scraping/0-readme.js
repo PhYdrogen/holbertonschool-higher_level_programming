d783faaad8e4836bcc5c6d03de06cea04b42342f
Gabriel <fossecruor@gmail.com>
2023-04-17 01:34:54 -0700

    Not async function now and better than ever

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
+const fs = require("fs");
 
-//const { read } = require("fs");
-
-const fs = request('fs').promises;
-
-async function readFile(file) {
-    try {
-        const data = await fs.readFile(file);
-        console.log(data.toString());
-    } catch (err) {
-        console.log(err);
-    }
+function readFile(file) {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) console.log(err);
+        console.log(data)
+    });
 }
 
-readFile(process.argv[1])
+readFile(process.argv[2]);
