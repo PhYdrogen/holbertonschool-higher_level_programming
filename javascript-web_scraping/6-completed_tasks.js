4383fc5605fb362ac42f98437707a857f79e978e
Gabriel <fossecruor@gmail.com>
2023-04-17 04:09:01 -0700

    Fix semistandard ref Leonidas

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,18 +2,17 @@
 const req = require('request');
 
 req(process.argv[2], (err, result, body) => {
-  let stats = {}; 
+  if (err) console.log(err.message);
+  const stats = {};
   const data = JSON.parse(body);
   data.forEach(obj => {
     if (obj.completed === true) {
-      if (isNaN(stats[obj.userId])){
+      if (isNaN(stats[obj.userId])) {
         stats[obj.userId] = 1;
-      }
-      else {
+      } else {
         stats[obj.userId] = stats[obj.userId] + 1;
       }
     }
-    
   });
   console.log(stats);
 });
