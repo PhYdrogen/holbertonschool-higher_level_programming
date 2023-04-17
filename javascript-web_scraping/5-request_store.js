9e08094fece94f145b3c2eec2b678bb31d4b9c62
Gabriel <fossecruor@gmail.com>
2023-04-17 03:33:57 -0700

    updated gitignore and add content for file 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const req = require('request');
+const fs = require('fs');
+
+req(process.argv[2], (err, res) => {
+  if (err) console.log(err);
+  const data = res.body;
+  fs.writeFile(process.argv[3], data, 'utf8', (err, result) => {
+    if (err) console.log(err);
+  })
+});
