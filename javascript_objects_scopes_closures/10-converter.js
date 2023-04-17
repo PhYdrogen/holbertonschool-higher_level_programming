a569cec4f4402203286f52ae95b012f6c653a27f
PhYdrogen <Fossecruor@gmail.com>
2023-04-17 09:30:48 +0200

    cleaned my file
    removed console.log

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-// Comprendre la closure c'est pas si simple
 function converter (base) {
   function myConverter (num) {
-    // console.log('my:' + num);
     return num.toString(base);
   }
-  // console.log('conv: '+ base);
   return myConverter;
 }
 module.exports = { converter };
