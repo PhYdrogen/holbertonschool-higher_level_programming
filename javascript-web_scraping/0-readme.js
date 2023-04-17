35a0a4970a4d45414e4c2714887b57f63ff72bf7
Gabriel <fossecruor@gmail.com>
2023-04-17 01:37:01 -0700

    Fixed semistandard issue

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
-function readFile(file) {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) console.log(err);
-        console.log(data)
-    });
+function readFile (file) {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+  });
 }
 
 readFile(process.argv[2]);
