6e2b90c9413a8e41b29f35aa682e01b70dc7d12c
Gabriel <fossecruor@gmail.com>
2023-04-17 01:44:57 -0700

    Adding content for file 1, unused variable result

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+function writeFile (file, text) {
+  fs.writeFile(file, text, (err, result) => {
+    if (err) console.log(err);
+  });
+}
+
+writeFile(process.argv[2], process.argv[3]);
