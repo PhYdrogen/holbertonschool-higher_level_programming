2509487e67bf18b029a6f7d869a3965c9e713269
Gabriel <fossecruor@gmail.com>
2023-04-17 02:21:24 -0700

    Adding content for file 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const req = require('request');
+let count = 0;
+
+req(process.argv[2], (err, res) => {
+  if (err) console.log(err);
+  const arr = JSON.parse(res.body).results;
+  arr.forEach(item => {
+    item.characters.forEach(itemofitem => {
+      if (itemofitem === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count = count + 1;
+      }
+    });
+  });
+  console.log(count);
+});
